a822427eaa355873bf9644b53f9472284ce2b306
bart batalinski <bartbatalinski@gmail.com>
2021-12-14 07:05:56 -0500

    added click event to hamburger menu, toggled some classes :moon::santa:

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('its woooooorrrrrrkiiiiing');
+// I want to add a click event to the hamburger button
+// this click should trigger some styles to kick in
+// side menu should slide in 
+
+// lets capture the hamburger in a variable first perhaps
+const hamburger = document.querySelector('button.hamburger');
+
+// next lets add a click event to the hamburger
+hamburger.addEventListener('click', () => {
+    // we want to trigger the menu to display: block
+    const menu = document.querySelector('ul.mobile');
+    const specialLis = document.querySelectorAll('ul.mobile li.special-mobile');
+    menu.classList.toggle('menu-pop-out');
+    // specialLis.classList.remove('special-mobile');
+    specialLis.forEach((item) => {
+        item.classList.remove('special-mobile');
+    });
+})
