fac2afa741411df0b40ebe46db3152df9f891e92
bart batalinski <bartbatalinski@gmail.com>
2021-12-14 19:27:24 -0500

    clicking outside of nav will close it

--- main.js
+++ main.js
@@ -6,9 +6,24 @@ const creativeApp = {};
 
 // lets capture the hamburger in a variable first perhaps
 creativeApp.hamburger = document.querySelector('button.hamburger');
+creativeApp.html = document.documentElement;
 
 creativeApp.toggleClass = (node, styleClass) => {
     node.classList.toggle(styleClass);
+}
+
+creativeApp.closeMenu = (event) => {
+    // get the event path
+    const path = event.composedPath();
+    // check if it has the menu element
+    if (path.some(elem => elem.id === 'myMenuId')) {
+        // terminate this function if it does
+        return
+    }
+    const menu = document.querySelector('ul.mobile');
+    menu.classList.remove('clicked');
+    creativeApp.toggleClass(creativeApp.hamburger, 'hamburger-transformed');
+    creativeApp.html.removeEventListener('click', creativeApp.closeMenu);
 }
 
 // next lets add a click event to the hamburger
@@ -26,6 +41,14 @@ creativeApp.hamburger.addEventListener('click', () => {
     // we want to create a function that handles the toggling of classes
     creativeApp.toggleClass(menu, 'clicked');
     creativeApp.toggleClass(creativeApp.hamburger, 'hamburger-transformed');
+
+    // if a user clicks outside of menu and the menu is open then attach an event listener to the html document in order to close it, otherwise remove the event listener
+    // solution provided by Anthony Oyathelemhi link:https://medium.com/@frontendtony/how-to-close-a-navigation-menu-when-the-page-is-clicked-3c607065a379
+    if(menu.classList[3]) {
+        creativeApp.html.addEventListener('click', creativeApp.closeMenu);
+    } else {
+        creativeApp.html.removeEventListener('click', creativeApp.closeMenu);
+    }
 });
 };
 
